e5f59f19e05006500f0d1f6aea3b51669faa96c9
HoangDung3009 <hoangdung123asd@gmail.com>
2022-03-09 23:52:27 +0700

    Edited

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path(r'<int:product_id>/', views.details, name='details')
 ]
--- store/views.py
+++ store/views.py
@@ -8,3 +8,9 @@ def index(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'store/index.html', context)
+
+
+def details(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {'product_details': product}
+    return render(request, 'store/product_details.html', context)
